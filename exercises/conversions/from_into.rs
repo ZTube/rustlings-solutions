3f570f14d2153d3403fc08d736ac5b0b9b766acf
ZTube <ztube@users.noreply.github.com>
2022-07-08 00:46:56 +0200

    Update the rustlings exercises with my approaches/solutions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() != 0 {
+            let mut split = s.split(',');
+            if let Some(name) = split.next() {
+                if name.len() != 0 {
+                    if let Some(age) = split.next() {
+                        if let Ok(age) = age.parse::<usize>() {
+                            if split.next().is_none() {
+                                return Person {
+                                    name: String::from(name),
+                                    age
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Person::default()
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -26,8 +26,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -41,6 +39,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 { return Err(ParsePersonError::Empty); }
+
+        let split = s.split(',');
+        if split.count() != 2 { return Err(ParsePersonError::BadLen); }
+        let mut split = s.split(',');
+        let name = split.next().unwrap();
+
+        if name.len() == 0 { return Err(ParsePersonError::NoName); }
+
+        let age = split.next().unwrap().parse::<usize>()
+            .map_err(|err| ParsePersonError::ParseInt(err))?;
+
+        Ok(Person {
+            name: String::from(name),
+            age
+        })
+
     }
 }
 
